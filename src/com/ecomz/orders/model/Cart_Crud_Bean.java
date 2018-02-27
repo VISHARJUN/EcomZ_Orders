35088afad5afb1605b6fa8e99b7b88f3bb49d488
VISHARJUN <35761183+VISHARJUN@users.noreply.github.com>
2018-02-28 00:18:37 +0530

    Add files via upload

--- src/com/ecomz/orders/model/Cart_Crud_Bean.java
+++ src/com/ecomz/orders/model/Cart_Crud_Bean.java
@@ -47,17 +47,17 @@ public class Cart_Crud_Bean {
 		}
 	}
 
-	public void updateCartItem(String strItemIndex, String strQuantity) {
+	public void updateCartItem(String ItemIndex, String strQuantity) {
 		double totalCost = 0.0;
 		double unitCost = 0.0;
 		int quantity = 0;
-		int ItemIndex = 0;
+		int itemIndex = 0;
 		Cart_item_Bean cartItem = null;
 		try {
-			ItemIndex = Integer.parseInt(strItemIndex);
+			itemIndex = Integer.parseInt(ItemIndex);
 			quantity = Integer.parseInt(strQuantity);
 			if (quantity > 0) {
-				cartItem = (Cart_item_Bean) cartItems.get(ItemIndex - 1);
+				cartItem = (Cart_item_Bean) cartItems.get(itemIndex - 1);
 				unitCost = cartItem.getUnitCost();
 				totalCost = unitCost * quantity;
 				cartItem.setQuantity(quantity);
@@ -68,6 +68,43 @@ public class Cart_Crud_Bean {
 			System.out.println("Item Can't be updated due to the Error : " + exception.getMessage());
 			exception.printStackTrace();
 		}
+	}
+
+	public Cart_item_Bean getCartItem(int itemIndex) {
+		Cart_item_Bean Cartitem = null;
+		if (cartItems.size() > itemIndex) {
+			Cartitem = (Cart_item_Bean) cartItems.get(itemIndex);
+		}
+		return Cartitem;
+	}
+
+	public void addCartItem(String productId, String productDescription, String unitCost, String quantity) {
+		double TotalCost = 0.0;
+		double UnitCost = 0.0;
+		int Quantity = 0;
+		Cart_item_Bean Cartitem = new Cart_item_Bean();
+		try {
+			UnitCost = Double.parseDouble(unitCost);
+			Quantity = Integer.parseInt(quantity);
+			if (Quantity > 0) {
+				TotalCost = UnitCost * Quantity;
+				Cartitem.setProductId(productId);
+				Cartitem.setProductDescription(productDescription);
+				Cartitem.setUnitCost(UnitCost);
+				Cartitem.setQuantity(Quantity);
+				Cartitem.setTotalCost(TotalCost);
+				cartItems.add(Cartitem);
+				CalcualteOrderTotal();
+			}
+
+		} catch (NumberFormatException nfe) {
+			System.out.println("Error while parsing from String to primitive types: " + nfe.getMessage());
+			nfe.printStackTrace();
+		}
+	}
+
+	public void addCartItem(Cart_item_Bean cartItem) {
+		cartItems.add(cartItem);
 	}
 
 }
